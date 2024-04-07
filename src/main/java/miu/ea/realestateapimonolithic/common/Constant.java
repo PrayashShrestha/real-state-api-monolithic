8d1c89c192bcdd161bbe978d540721dff142ff05
prayash27 <prayash.shrestha@wiseyak.com>
2024-04-07 01:19:55 -0500

    initial application and docker-compose file setup

--- src/main/java/miu/ea/realestateapimonolithic/common/Constant.java
+++ src/main/java/miu/ea/realestateapimonolithic/common/Constant.java
@@ -7,6 +7,4 @@ public class Constant {
     public static final String PROPERTY_URL_PREFIX = API_URL_v1_PREFIX + "/properties";
     public static final String ADMIN_URL_PREFIX = API_URL_v1_PREFIX + "/admin";
     public static final String BUYER_URL_PREFIX = API_URL_v1_PREFIX + "/buyers";
-
-
 }
