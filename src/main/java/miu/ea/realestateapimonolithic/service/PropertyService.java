81f907709fe46cb086d1917e7d75158996065ae7
tanushaayer <tanushastan@gmail.com>
2024-04-07 13:55:18 -0500

    Implements CRUD for property

--- src/main/java/miu/ea/realestateapimonolithic/service/PropertyService.java
+++ src/main/java/miu/ea/realestateapimonolithic/service/PropertyService.java
@@ -1,6 +1,29 @@
 package miu.ea.realestateapimonolithic.service;
 
+import miu.ea.realestateapimonolithic.common.ListingStatusEnum;
+import miu.ea.realestateapimonolithic.dto.PropertyDto;
+import miu.ea.realestateapimonolithic.exception.PropertyException;
+import miu.ea.realestateapimonolithic.model.Property;
+import miu.ea.realestateapimonolithic.model.Role;
+import miu.ea.realestateapimonolithic.model.User;
+
+import java.util.List;
+
 public interface PropertyService {
+
+    void save(Property property);
+
+    List<PropertyDto> findAll();
+
+    List<PropertyDto> findAllByUserAndListingStatus(Long userId);
+
+    void deleteById(Long id);
+
+    void delete(PropertyDto property);
+
+    List<PropertyDto> findAllByListingStatus();
+
+    PropertyDto findById(Long id) throws PropertyException;
 
     void approveProperty(Long propertyId);
     void rejectProperty(Long propertyId);
--- src/main/java/miu/ea/realestateapimonolithic/service/impl/PropertyServiceImpl.java
+++ src/main/java/miu/ea/realestateapimonolithic/service/impl/PropertyServiceImpl.java
@@ -2,11 +2,22 @@ package miu.ea.realestateapimonolithic.service.impl;
 
 import lombok.RequiredArgsConstructor;
 import miu.ea.realestateapimonolithic.common.ListingStatusEnum;
+import miu.ea.realestateapimonolithic.common.RoleEnum;
+import miu.ea.realestateapimonolithic.dto.PropertyDto;
+import miu.ea.realestateapimonolithic.exception.PropertyException;
+import miu.ea.realestateapimonolithic.exception.UserException;
+import miu.ea.realestateapimonolithic.mapper.PropertyMapper;
 import miu.ea.realestateapimonolithic.model.Property;
+import miu.ea.realestateapimonolithic.model.Role;
+import miu.ea.realestateapimonolithic.model.User;
 import miu.ea.realestateapimonolithic.repository.PropertyRepository;
+import miu.ea.realestateapimonolithic.repository.UserRepository;
 import miu.ea.realestateapimonolithic.service.PropertyService;
 import org.slf4j.Logger;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -14,7 +25,88 @@ public class PropertyServiceImpl implements PropertyService {
     private static final Logger LOG = org.slf4j.LoggerFactory.getLogger(PropertyServiceImpl.class);
 
     private final PropertyRepository propertyRepository;
+    private final UserRepository userRepository;
 
+
+    @Override
+    public void save(Property property) {
+        propertyRepository.save(property);
+    }
+
+    @Override
+    public List<PropertyDto> findAll() {
+        return propertyRepository.findAll()
+                 .stream()
+                 .map(PropertyMapper.MAPPER::mapToPropertyDto)
+                 .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<PropertyDto> findAllByUserAndListingStatus(Long userId) throws UserException {
+        RoleEnum role;
+        if(userRepository.findById(userId).isPresent()){
+            User user = userRepository.findById(userId).get();
+            role = user.getRole().getRole();
+            List<Property> properties = propertyRepository.findAllByUser(userId);
+            if (role == RoleEnum.ADMIN){
+                return this.findAll();
+            } else if (role == RoleEnum.BUYER) {
+                return this.findAllByListingStatus();
+            }else{
+                return properties.stream()
+                        .map(PropertyMapper.MAPPER::mapToPropertyDto)
+                        .collect(Collectors.toList());
+            }
+
+        }
+        else{
+            throw new UserException("User Not Found");
+        }
+    }
+
+
+    @Override
+    public void deleteById(Long id) {
+        if(propertyRepository.findById(id).isPresent()){
+            propertyRepository.deleteById(id);
+        }else {
+            try {
+                throw new PropertyException("Property Not Found");
+            } catch (PropertyException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+    }
+
+    @Override
+    public void delete(PropertyDto propertyDto) {
+        Property property = PropertyMapper.MAPPER.mapToProperty(propertyDto);
+        propertyRepository.delete(property);
+
+    }
+
+    @Override
+    public List<PropertyDto> findAllByListingStatus() {
+        return propertyRepository.findAllByListingStatus(ListingStatusEnum.APPROVED)
+                .stream()
+                .map((PropertyMapper.MAPPER::mapToPropertyDto))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public PropertyDto findById(Long id) {
+        if(propertyRepository.findById(id).isPresent()){
+            Property p = propertyRepository.findById(id).get();
+            return PropertyMapper.MAPPER.mapToPropertyDto(p);
+        } else {
+            try {
+                throw new PropertyException("Property Doesn't Exist");
+            } catch (PropertyException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 
     @Override
     public void approveProperty(Long propertyId) {
