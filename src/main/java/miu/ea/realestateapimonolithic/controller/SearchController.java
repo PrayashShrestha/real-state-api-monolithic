ee5e8d43f0a9a6b539ac8f1287518dcbcb5761b6
But Bui <butbuiapp@gmail.com>
2024-04-09 15:26:33 -0500

    Update average rating for Agent when there is new review, that is used for searching agents by rating

--- src/main/java/miu/ea/realestateapimonolithic/controller/SearchController.java
+++ src/main/java/miu/ea/realestateapimonolithic/controller/SearchController.java
@@ -2,18 +2,16 @@ package miu.ea.realestateapimonolithic.controller;
 
 import lombok.RequiredArgsConstructor;
 import miu.ea.realestateapimonolithic.common.Constant;
+import miu.ea.realestateapimonolithic.dto.AgentDto;
 import miu.ea.realestateapimonolithic.dto.AgentSearchRequest;
 import miu.ea.realestateapimonolithic.dto.PropertySearchRequest;
 import miu.ea.realestateapimonolithic.dto.SearchResponse;
-import miu.ea.realestateapimonolithic.model.Property;
 import miu.ea.realestateapimonolithic.service.AgentService;
 import miu.ea.realestateapimonolithic.service.PropertyService;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(Constant.SEARCH_URL_PREFIX)
@@ -34,5 +32,10 @@ public class SearchController {
         PageRequest pageRequest = PageRequest.of(searchRequest.getPageNumber()-1, searchRequest.getPageSize());
 
         return agentService.search(searchRequest, pageRequest);
+    }
+
+    @GetMapping("/agent/{userId}")
+    public ResponseEntity<AgentDto> getAgentById(@PathVariable Long userId) {
+        return new ResponseEntity<>(agentService.getAgentById(userId), HttpStatus.OK);
     }
 }
