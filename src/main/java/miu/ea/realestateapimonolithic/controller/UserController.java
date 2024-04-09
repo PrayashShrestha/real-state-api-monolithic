69ab866b4afd8e2b05884a181af9efc2e13f24b9
tanushaayer <tanushastan@gmail.com>
2024-04-08 20:59:50 -0500

    adds view review enspoint to user controller

--- src/main/java/miu/ea/realestateapimonolithic/controller/UserController.java
+++ src/main/java/miu/ea/realestateapimonolithic/controller/UserController.java
@@ -2,9 +2,11 @@ package miu.ea.realestateapimonolithic.controller;
 
 import lombok.RequiredArgsConstructor;
 import miu.ea.realestateapimonolithic.common.Constant;
+import miu.ea.realestateapimonolithic.dto.AgentReviewPreviewDto;
 import miu.ea.realestateapimonolithic.dto.PropertyDto;
 import miu.ea.realestateapimonolithic.dto.UserDto;
 import miu.ea.realestateapimonolithic.model.User;
+import miu.ea.realestateapimonolithic.service.AgentReviewService;
 import miu.ea.realestateapimonolithic.service.PropertyService;
 import miu.ea.realestateapimonolithic.service.UserService;
 import org.springframework.http.HttpStatus;
@@ -20,16 +22,29 @@ public class UserController {
 
     private final UserService userService;
     private final PropertyService propertyService;
+    private final AgentReviewService agentReviewService;
 
     @PostMapping
     public void saveUser(@RequestBody UserDto user) {
         userService.saveUser(user);
     }
 
-    @GetMapping("/{id}/property")
-    public ResponseEntity<List<PropertyDto>> getPropertyByUser(@PathVariable long id){
-        List<PropertyDto> properties = propertyService.findAllByUserAndListingStatus(id);
+    @GetMapping("/{userId}/property")
+    public ResponseEntity<List<PropertyDto>> getPropertyByUser(@PathVariable long userId){
+        List<PropertyDto> properties = propertyService.findAllByUserAndListingStatus(userId);
         return new ResponseEntity<>(properties, HttpStatus.OK);
+    }
+
+    @GetMapping("/reviews")
+    public ResponseEntity<List<AgentReviewPreviewDto>> getAllAgentReviews(){
+        List<AgentReviewPreviewDto> agentReviews = agentReviewService.getAllAgentReview();
+        return new ResponseEntity<>(agentReviews, HttpStatus.OK);
+    }
+
+    @GetMapping("/reviews/{agentId}")
+    public ResponseEntity<List<AgentReviewPreviewDto>> getAgentReviewById(@PathVariable Long agentId){
+        List<AgentReviewPreviewDto> agentReviews = agentReviewService.getAgentReviewsByAgentId(agentId);
+        return new ResponseEntity<>(agentReviews,HttpStatus.OK);
     }
 
 }
