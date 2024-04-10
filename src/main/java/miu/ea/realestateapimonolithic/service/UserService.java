977d6db98716d5706e30ccd2bec943c77f3175e1
But Bui <butbuiapp@gmail.com>
2024-04-10 15:45:54 -0500

    Fixed merge issue

--- src/main/java/miu/ea/realestateapimonolithic/service/UserService.java
+++ src/main/java/miu/ea/realestateapimonolithic/service/UserService.java
@@ -12,7 +12,6 @@ public interface UserService {
     void saveAdmin(AccountRegistrationRequest user);
 
     void save(User user);
-    User findUser(long id);
     List<UserResponseDto> findAllUsers();
     void updateUser(long id, UserUpdateDto user, MultipartFile profilePhoto);
 
