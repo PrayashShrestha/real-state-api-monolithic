77919cf84c165aba521551c7192c4338857a7e86
tanushaayer <tanushastan@gmail.com>
2024-04-09 19:41:30 -0500

    adds buyer's remove favorite property, adds global exception handler and handles add remove fav exceptions

--- src/main/java/miu/ea/realestateapimonolithic/service/impl/BuyerServiceImpl.java
+++ src/main/java/miu/ea/realestateapimonolithic/service/impl/BuyerServiceImpl.java
@@ -2,11 +2,15 @@ package miu.ea.realestateapimonolithic.service.impl;
 
 import lombok.RequiredArgsConstructor;
 import miu.ea.realestateapimonolithic.dto.PropertyDto;
+import miu.ea.realestateapimonolithic.exception.AlreadyExistException;
+import miu.ea.realestateapimonolithic.exception.InvalidInputException;
 import miu.ea.realestateapimonolithic.exception.NotFoundException;
 import miu.ea.realestateapimonolithic.mapper.PropertyMapper;
 import miu.ea.realestateapimonolithic.model.Buyer;
 import miu.ea.realestateapimonolithic.model.BuyerPreference;
+import miu.ea.realestateapimonolithic.model.Property;
 import miu.ea.realestateapimonolithic.repository.BuyerRepository;
+import miu.ea.realestateapimonolithic.repository.PropertyRepository;
 import miu.ea.realestateapimonolithic.service.BuyerService;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +21,7 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class BuyerServiceImpl implements BuyerService {
     private final BuyerRepository buyerRepository;
+    private final PropertyRepository propertyRepository;
 
     @Override
     public Buyer getBuyerById(Long id) {
@@ -37,8 +42,14 @@ public class BuyerServiceImpl implements BuyerService {
     @Override
     public void addFavouriteProperty(Long buyerId, PropertyDto propertyDto) {
         Buyer buyer = buyerRepository.findById(buyerId).orElseThrow(() -> new NotFoundException("Buyer Not Found : " + buyerId));
+        List<Property> favorites = buyer.getFavouriteProperties();
+        if(favorites.contains(PropertyMapper.MAPPER.mapToProperty(propertyDto))){
+            throw new AlreadyExistException("Property is already on favorites");
+        }
+        else{
         buyer.getFavouriteProperties().add(PropertyMapper.MAPPER.mapToProperty(propertyDto));
         buyerRepository.save(buyer);
+        }
     }
 
     @Override
@@ -47,5 +58,23 @@ public class BuyerServiceImpl implements BuyerService {
         return buyer.getFavouriteProperties().stream()
                 .map(PropertyMapper.MAPPER::mapToPropertyDto)
                 .collect(Collectors.toList());
+    }
+
+    @Override
+    public void removeFavouriteProperty(Long buyerId, PropertyDto propertyDto) {
+        Buyer buyer = buyerRepository.findById(buyerId).orElseThrow(() -> new NotFoundException("Buyer Not Found : " + buyerId));
+        List<Property> favorites = buyer.getFavouriteProperties();
+        if(propertyRepository.findById(propertyDto.getId()).isPresent()){
+            Property userSelectedProperty =propertyRepository.findById(propertyDto.getId()).get();
+            if(favorites.contains(userSelectedProperty)){
+                favorites.remove(userSelectedProperty);
+                buyerRepository.save(buyer);
+             }
+            else{
+                throw new InvalidInputException("The property isn't a favorited property");
+            }
+        }else{
+            throw new NotFoundException("Property Not Found");
+        }
     }
 }
