bb79a20b0225885eb24d79064c6d65438ed93e9d
But Bui <butbuiapp@gmail.com>
2024-04-10 16:18:42 -0500

    Fixed add qualifications and languages of agent

--- src/main/java/miu/ea/realestateapimonolithic/repository/LanguageRepository.java
+++ src/main/java/miu/ea/realestateapimonolithic/repository/LanguageRepository.java
@@ -4,10 +4,9 @@ import miu.ea.realestateapimonolithic.model.Language;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface LanguageRepository extends JpaRepository<Language, Long> {
-    public Optional<Language> findByLanguage(String language);
+    Optional<Language> findByAgentIdAndLanguage(Long userId, String language);
 }
